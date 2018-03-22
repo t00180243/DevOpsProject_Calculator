3ecf1429e43b49cdedeefbb11b73d03bc352de9c
Aoife Horgan <t00180243@gmail.com>
2018-03-22 23:49:27 +0000

    Initial commit

---
+++ app/src/test/java/devops/aoifehorgan/me/devopsproject/CalcActivityTest.java
@@ -0,0 +1,32 @@
+package devops.aoifehorgan.me.devopsproject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Aoife Horgan on 22/03/2018.
+ */
+public class CalcActivityTest {
+    @Test
+    public void onCreate() throws Exception {
+    }
+
+    @Test
+    public void addC() throws Exception {
+        assertEquals(4,2+2);
+    }
+
+    @Test
+    public void subtract() throws Exception {
+    }
+
+    @Test
+    public void divide() throws Exception {
+    }
+
+    @Test
+    public void multiply() throws Exception {
+    }
+
+}
